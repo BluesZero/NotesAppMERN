8acdcc8d8a69019a16fb18c2f40d96fe20057e60
BluesZero <50782397+BluesZero@users.noreply.github.com>
2020-10-27 16:07:36 -0600

    Add files via upload

---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const app = require('./app');
+require('./database');
+
+async function main(){
+    // app.listen(4000, () => console.log('server on port 4000'));
+
+    await app.listen(app.get('port'));
+    console.log('server on port ', app.get('port'));
+}
+
+main();
